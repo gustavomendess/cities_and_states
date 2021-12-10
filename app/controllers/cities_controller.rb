a46473da3b5aba416bb02f127e568b5e1fa0fbc7
gustavomendess <gustavocod13@outlook.com>
2021-12-10 01:13:21 -0300

    Refactor controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,8 +1,11 @@
 class CitiesController < ApplicationController
   def index
+    data(params)
+  end
+
+  def data(params)
     @states = State.all
     @cities = CitiesFetcher.new.filter(params)
-
     @searched_name = params[:city][:name].strip if params[:city].present?
     @searched_state = State.find(params[:state][:id]) if params[:state].present? && params[:state][:id].present?
   end
