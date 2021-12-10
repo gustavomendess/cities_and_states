c0688b44acf21f5f3343433caf9921126b9fb35d
gustavomendess <gustavocod13@outlook.com>
2021-12-10 00:40:37 -0300

    fix(corrige scope)

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,5 +6,5 @@ class City < ApplicationRecord
 
   scope :by_state_id, ->(state_id) { where(state_id: state_id) }
   scope :by_name, ->(name) { where("UNACCENT(name) ILIKE ?", "%#{I18n.transliterate(name)}%") }
-  scope :by_state_and_name, ->(state_id, name) { where(state_id: state_id).where("name ILIKE ?", "%#{I18n.transliterate(name)}%") }
+  scope :by_state_and_name, ->(state_id, name) { where(state_id: state_id).where("UNACCENT(name) ILIKE ?", "%#{I18n.transliterate(name)}%") }
 end
